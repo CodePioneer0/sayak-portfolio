3f481de6569d66b099878a3e231b64295e034613
CodePioneer0 <153994681+CodePioneer0@users.noreply.github.com>
2025-06-22 16:03:21 +0530

    Update configuration for custom domain sayaksen.me

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
   },
   plugins: [react()],
-  base : "/sayak-portfolio",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
